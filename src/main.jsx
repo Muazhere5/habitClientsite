539b6cbe064354ede8c7be8bc2f471da5af2f8cb
Muazhere5 <muazctg07@gmail.com>
2025-11-14 17:23:45 +0600

    Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import router from './router/router.jsx'; 
+import './index.css';
+import AuthProvider from './providers/AuthProvider';
+import { Toaster } from 'react-hot-toast'; 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} /> 
+      {/* Global Toaster setup for success/error messages */}
+      <Toaster position="top-center" reverseOrder={false} />
+    </AuthProvider>
+  </React.StrictMode>,
+);
